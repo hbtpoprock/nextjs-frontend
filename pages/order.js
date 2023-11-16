cf56d7a4c744b1a726d5a980f47dfad60a39a897
hbtpoprock <krisnu.sut@gmail.com>
2023-11-16 22:16:48 +0700

    handleSubmit call create order api

--- pages/order.js
+++ pages/order.js
@@ -1,11 +1,19 @@
-// pages/order.js
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import ProtectedRoute from "../components/ProtectedRoute";
+import { Checkbox, Button } from "antd";
+
+const items = {
+  1: { name: "Item 1", price: 3 },
+  2: { name: "Item 2", price: 4 },
+  3: { name: "Item 3", price: 1 },
+  4: { name: "Item 4", price: 2 },
+};
 
 const OrderPage = () => {
   const router = useRouter();
   const [selectedItems, setSelectedItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
   const [itemToggles, setItemToggles] = useState({
     1: false,
     2: false,
@@ -24,7 +32,7 @@ const OrderPage = () => {
     }));
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const selectedItemsArray = Object.entries(itemToggles)
       .filter(([itemId, isSelected]) => isSelected)
       .map(([itemId]) => Number(itemId));
@@ -32,8 +40,47 @@ const OrderPage = () => {
     // Update the state with the selected items
     setSelectedItems(selectedItemsArray);
 
-    // Log the selected items to the console (you can replace this with your desired logic)
+    // Calculate total price
+    const newTotalPrice = selectedItemsArray.reduce(
+      (total, itemId) => total + items[itemId].price,
+      0
+    );
+
+    // Update the state with the total price
+    setTotalPrice(newTotalPrice);
+
+    // Log the selected items and total price to the console
     console.log("Selected Items:", selectedItemsArray);
+    console.log("Total Price:", newTotalPrice);
+
+    try {
+      // Call your create order API endpoint
+      const response = await fetch("http://localhost:8000/api/orders", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Include the access token in the headers
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify({
+          food_ids: selectedItemsArray,
+          total_price: newTotalPrice,
+        }),
+      });
+
+      if (response.ok) {
+        // Clear the access token from localStorage
+        const data = await response.json();
+        console.log("data", data);
+        // Redirect to the login page
+        // router.push("/login");
+      } else {
+        // Handle logout error
+        console.error("create order failed:", await response.text());
+      }
+    } catch (error) {
+      console.error("Error during create order:", error);
+    }
   };
 
   const handleLogout = async () => {
@@ -65,55 +112,44 @@ const OrderPage = () => {
 
   return (
     <ProtectedRoute>
-      <div>
+      <div style={{ padding: "20px", maxWidth: "600px", margin: "auto" }}>
+        <div style={{ marginBottom: "10px", textAlign: "right" }}>
+          <Button onClick={handleLogout}>Logout</Button>
+        </div>
         <h1>Order Page</h1>
         {/* Toggle buttons for items */}
-        <div>
-          <label>
-            Item 1
-            <input
-              type="checkbox"
-              checked={itemToggles[1]}
-              onChange={() => handleToggle(1)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Item 2
-            <input
-              type="checkbox"
-              checked={itemToggles[2]}
-              onChange={() => handleToggle(2)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Item 3
-            <input
-              type="checkbox"
-              checked={itemToggles[3]}
-              onChange={() => handleToggle(3)}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Item 4
-            <input
-              type="checkbox"
-              checked={itemToggles[4]}
-              onChange={() => handleToggle(4)}
-            />
-          </label>
+        {Object.entries(items).map(([itemId, item]) => (
+          <div key={itemId}>
+            <Checkbox
+              checked={itemToggles[itemId]}
+              onChange={() => handleToggle(itemId)}
+            >
+              {item.name} - {item.price} USD
+            </Checkbox>
+          </div>
+        ))}
+
+        {/* Submit button on a new line */}
+        <div style={{ marginTop: "10px" }}>
+          <Button type="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
         </div>
 
-        {/* Submit button */}
-        <button onClick={handleSubmit}>Submit</button>
+        {/* Display selected items and total price */}
+        {selectedItems.length > 0 && (
+          <div style={{ marginTop: "20px" }}>
+            <h3>Selected Items:</h3>
+            <ul>
+              {selectedItems.map((itemId) => (
+                <li key={itemId}>{items[itemId].name}</li>
+              ))}
+            </ul>
+            <h3>Total Price: {totalPrice} USD</h3>
+          </div>
+        )}
 
         {/* Your order page content goes here */}
-        <button onClick={handleLogout}>Logout</button>
       </div>
     </ProtectedRoute>
   );
